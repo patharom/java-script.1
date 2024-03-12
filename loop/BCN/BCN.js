abc71cc2b9df55b2bca68f7d9b0113e8af623047
patharom <139317406+patharom@users.noreply.github.com>
2024-03-12 11:53:59 +0530

    Add files via upload

---
+++ loop/BCN/BCN.js
@@ -0,0 +1,125 @@
+/* Break , Continue and Nested Statements */
+
+// {
+//     for (let counter = 0; counter <= 10; counter++) {
+//          if (counter == 6) {
+//               break;
+//          }
+//          document.write(counter);
+//          document.write("<br>");
+//     }
+// }
+
+// {
+//     for (let counter =1; counter <=20; counter++) {
+//         if (counter == 10) {
+//             break;
+//         }
+//         document.write(counter);
+//         document.write("<br>");
+//     }
+// }
+
+
+// {
+//     for (let counter = 5; counter <=50; counter--) {
+//         if (counter == 20) {
+//             break;
+//         }
+//         document.write(counter);
+//         document.write("<br>");
+//     }
+// }
+
+// {
+//     for(let counter = 1; counter <= 10 ; counter++){
+//     if(counter == 5){
+//          continue;
+//     }
+//     if(counter == 6){
+//          continue;
+//     }
+//     document.write(counter);
+//     document.write("<br>");
+//     }
+// }
+
+// {
+//     for(let counter = 1; counter <=20; counter++){
+//         if(counter == 8){
+//             continue;
+//         }
+//         if(counter == 15){
+//             continue;
+//         }
+//         document.write(counter);
+//         document.write("<br>");
+//     }
+// }
+
+// {
+//     for(let counter = 10; counter <=50; counter++){
+//         if(counter == 30){
+//             continue;
+//         }
+//         if(counter == 50){
+//             continue;
+//         }
+//         document.write(counter);
+//         document.write("<br>");
+//     }
+// }
+
+
+// {
+//     link: for(let counter = 1; counter <= 10 ; counter++){
+
+//     document.write(counter);
+//     document.write("<br>");
+
+//     for(let counter2 = 1; counter2 < 4; counter2++){
+
+//          if(counter == 3){
+//          break link;
+//         }
+//          document.write("Hello");
+//          document.write("<br>");
+//     }
+//     }
+// }
+
+
+// {
+//     link: for(let counter = 1; counter <= 50 ; counter++){
+
+//     document.write(counter);
+//     document.write("<br>");
+
+//     for(let counter2 = 5; counter2 < 15; counter2++){
+
+//          if(counter == 4){
+//          break link;
+//         }
+//          document.write("Hello");
+//          document.write("<br>");
+//     }
+//     }
+// }
+
+// {
+//     link: for(let counter = 1; counter <= 30; counter++){
+//         document.write(counter);
+//         document.write("<br>");
+
+//         for(let counter2 = 6; counter2 < 25; counter2++){
+
+//             if(counter == 5){
+//                 break link;
+//             }
+//             document.write("hello");
+//             document.write("<br>");
+//         }
+//     }
+// }
+
+
