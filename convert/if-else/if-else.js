7d1f3b77760c5399fe8fed2b7e36f654aff97366
patharom <139317406+patharom@users.noreply.github.com>
2024-03-15 10:52:16 +0530

    Add files via upload

---
+++ convert/if-else/if-else.js
@@ -0,0 +1,144 @@
+/*  JavaScript Statement */
+
+/* if...else statement */
+
+// let age = 1
+// if(age >= 18){
+//    console.log("You Are Aligible");
+// }
+// else{
+//    console.log("Not Aligible");
+// }
+// console.log("Hello");
+
+
+// let  = 2
+// if(age >= 20){
+//    console.log("you are jay");
+// }
+// else{
+//    console.log("not jay");
+// }
+// console.log("Hello");
+
+
+// let = 3
+// if(age <= 25){
+//    console.log("you are rahul");
+// }
+// else{
+//    console.log("yes rahul");
+// }
+// console.log("hello");
+
+
+/* if...else if... statement */
+
+// {
+//    var book ='history';
+  
+//    if( book === 'history' ) {
+//       document.write("<b>History Book</b>");
+//    }
+
+//    else if( book == "maths" ) {
+//       document.write("<b>Maths Book</b>");
+//    }
+
+//    else if( book == "economics" ) {
+//       document.write("<b>Economics Book</b>");
+//    }
+//    else {
+//       document.write("<b>Unknown Book</b>");
+//    }
+// }
+
+
+// {
+//    var icecream ='chocolate';
+
+//    if(icecream === 'chocolate') {
+//       document.write("chocolate icecream");
+//    }
+
+//    else if(icecream == 'venila') {
+//       document.write("venila icecream");
+//    }
+
+//    else if(icecream == 'stobary') {
+//       document.write("stobary icecream");
+//    }
+// }
+
+// {
+//    var watch = 'tag';
+
+//    if(watch === 'tag') {
+//       document.write("tag watch");
+//    }
+
+//    else if(watch == 'jacob&co') {
+//       document.write("jacob&co watch");
+//    }
+
+//    else if(watch == 'adumars pigget') {
+//       document.write("adumars pigget watch");
+//    }
+// }
+
+/* switch statement */
+
+// {
+//    let input = 0;
+//    switch(input){
+//       case 1:
+//          document.write("Continue...");
+//          break;
+//       case "y":
+//          document.write("Continue...");
+//          break;
+//       case "yes":
+//          document.write("Continue...");
+//          break;
+//       case 0:
+//          document.write("End...");
+//          break;
+//       case "n":
+//          document.write("End...");
+//          break;
+//       case "no":
+//          document.write("End...");
+//          break;
+//       default:
+//          document.write("Wrong input");
+//    }
+// }
+
+//  the grade for input marks
+
+
+// var mark = prompt()
+
+// if(mark <= 35){
+//     console.log("failed");
+// }
+
+// else if(mark >= 36  &&  mark <= 55){
+//     console.log("E gread");
+// }
+
+// else if(mark >= 56 &&  mark <= 59){
+//         console.log("D grad gred");
+// }
+
+// else if(mark >= 60  && mark <= 80){
+//     console.log("B gread");
+// }
+
+// else{
+//     console.log("A gread");
+// }
+
+
+
+
